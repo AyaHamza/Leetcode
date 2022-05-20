0641eefcade3fce4242406a208421a9fae67b600
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-05-21 00:31:55 +0200

    Time: 6 ms (36.57%), Space: 7.8 MB (41.60%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int  n,m,ans=0;
     int dp[101][101];
-    int Move(int i, int j,vector <vector<int>>& grid) {
+    vector<vector<int>>grid;
+    int Move(int i, int j) {
         int &ways=dp[i][j];
         if(grid[i][j])return 0;
         (i==0&&j==0&&!grid[0][0])?ways=1:ways=0;
         if(i-1>=0&&!grid[i-1][j])
-            dp[i-1][j]==-1?ways+=Move(i-1,j,grid):ways+=dp[i-1][j];
+            dp[i-1][j]==-1?ways+=Move(i-1,j):ways+=dp[i-1][j];
         if(j-1>=0&&!grid[i][j-1])
-            dp[i][j-1]==-1?ways+=Move(i,j-1,grid):ways+=dp[i][j-1];
+            dp[i][j-1]==-1?ways+=Move(i,j-1):ways+=dp[i][j-1];
         return dp[i][j];
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         memset(dp,-1,sizeof dp);
-        return Move(obstacleGrid.size()-1,obstacleGrid[0].size()-1,obstacleGrid);
+        grid=obstacleGrid;
+        return Move(obstacleGrid.size()-1,obstacleGrid[0].size()-1);
     }
 };
