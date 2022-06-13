62b558931c52c9fb696a6116aa753dbb1ca235e4
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-06-13 23:13:11 +0200

    Time: 13 ms (22.65%), Space: 8.6 MB (70.76%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    vector <vector<int>> T;
     int dp[205][205];
-    int Try(int x, int y) {
-        if (x == T.size())return 0;
+    int Try(int x, int y,vector <vector<int>> &triangle) {
+        if (x == triangle.size())return 0;
         if (dp[x][y] != -1)return dp[x][y];
         int &mn = dp[x][y] = 1e9;
-        mn = min(mn, Try(x + 1, y) + T[x][y]);
-        mn = min(mn, Try(x + 1, y + 1) + T[x][y]);
+        mn = min(mn, Try(x + 1, y,triangle) + triangle[x][y]);
+        mn = min(mn, Try(x + 1, y + 1,triangle) + triangle[x][y]);
         return mn;
     }
-
     int minimumTotal(vector <vector<int>> &triangle) {
-        T = triangle;
         memset(dp, -1, sizeof dp);
-        return Try(0, 0);
+        return Try(0, 0,triangle);
     }
 };
