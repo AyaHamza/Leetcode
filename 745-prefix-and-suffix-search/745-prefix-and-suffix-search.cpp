c7840688bf66689a49c07e5f7a93e554b790bed2
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-06-19 00:54:35 +0200

    Time: 893 ms (29.33%), Space: 183.5 MB (47.37%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class WordFilter {
+public:
+    unordered_map<string,int>dict;
+    WordFilter(vector <string> &words) {
+        for (int i = 0; i < words.size(); i++) {
+            vector <string> suf(words[i].size() + 1), pre(words[i].size() + 1);
+            string s = "", p = "",c;
+            pre[0] = p, suf[s.size()] = s;
+            for (int j = 0; j < words[i].size(); j++) {
+                p += words[i][j];
+                c=words[i][words[i].size()-j-1];
+                c+=s;
+                s=c;
+                pre[j + 1] = p, suf[words[i].size() - j - 1] = s;
+            }
+            for(int k=0;k<pre.size();k++)
+                for(int j=0;j<suf.size();j++)
+                    dict[pre[k]+"&"+suf[j]]=i;
+        }
+    }
+
+    int f(string prefix, string suffix) {
+        string s=prefix+"&"+suffix;
+        if( dict.find(s)==dict.end())
+            return -1;
+        return dict[s];
+    }
+
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(prefix,suffix);
+ */
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(prefix,suffix);
+ */
