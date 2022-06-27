211c75a44f6ddb7231b6d93012b03be1e94698dd
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-06-27 05:27:30 +0200

    Time: 27 ms (95.03%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,5 +1,10 @@
 class Solution {
+ 
 public:
+     Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
     int minPartitions(string n) {
         for(int i=1;i<n.size();i++)
            if(n[i]<n[i-1])
