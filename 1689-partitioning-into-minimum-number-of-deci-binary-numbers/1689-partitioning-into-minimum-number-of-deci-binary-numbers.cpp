db7178a59686e615e6e9e98f71a27f6e82ad471f
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-06-27 05:23:22 +0200

    Time: 66 ms (12.78%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        char ch='0';
-        for(int i=0;i<n.size();i++)
-            ch=max(ch,n[i]);
-        return ch-'0';
+        for(int i=1;i<n.size();i++)
+           if(n[i]<n[i-1])
+               n[i]=n[i-1];
+        return n[n.size()-1]-'0';
     }
 };
