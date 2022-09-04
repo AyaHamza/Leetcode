ecd0e337089c5200bb4a4d29a47a399b3f0fa0f0
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-09-04 23:44:47 +0200

    Time: 4 ms (83.69%), Space: 13.2 MB (65.40%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,31 +11,23 @@
  */
 class Solution {
 public:
-    vector<pair<pair<int,int>,int>>Order;
+    map<int,vector<pair<int,int>>>Order;
     void GetOrder(TreeNode* node,int r,int c){
         if(node==nullptr) return;
-        Order.push_back({{c,r},node->val});
+        Order[c].push_back({r,node->val});
         GetOrder(node->left,r+1,c-1);
-        GetOrder(node->right,r+1,c+1);
-                                        
+        GetOrder(node->right,r+1,c+1);                           
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>>ans;
-        vector<int>v;
         GetOrder(root,0,0);
-        sort(Order.begin(),Order.end());
-        int c=Order[0].first.first;
-        for(int i=0;i<Order.size();i++) {   
-                if(c==Order[i].first.first)v.push_back(Order[i].second);
-                else {
-                    if(v.size())ans.push_back(v);
-                    v.clear();
-                    v.push_back(Order[i].second);
-                    c=Order[i].first.first;
-                }
-            
+        vector<vector<int>>ans;
+        for(auto &it:Order){
+            sort(it.second.begin(),it.second.end());
+            vector<int>v(it.second.size());
+            for(int i=0;i<it.second.size();i++)
+                v[i]=it.second[i].second;
+            ans.push_back(v);
         }
-        if(v.size())ans.push_back(v);
         return ans;
     }
 };
