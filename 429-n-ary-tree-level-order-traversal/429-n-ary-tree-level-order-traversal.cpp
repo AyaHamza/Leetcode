24c04a3cb2259cfccdd4ea1c3092f9dd56e72e36
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-09-05 02:31:17 +0200

    Time: 46 ms (24.79%), Space: 12.6 MB (6.02%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -19,24 +19,24 @@ public:
 */
 
 class Solution {
-    vector<vector<int>>ans;
-    map<int,vector<int>>mp;
 public:
+    vector <vector<int>> ans;
+    map<int, vector<int>> mp;
     vector <vector<int>> levelOrder(Node *root) {
-        queue<pair<Node* ,int>>q;
-        q.push({root,0});
-        int Level=0;
-        Node* node;
-        while(q.size()){
-        
-            node=q.front().first;
-            Level=q.front().second;
+        queue <pair<Node *, int>> q;
+        q.push({root, 0});
+        int Level = 0;
+        Node *node;
+        while (q.size()) {
+            node = q.front().first;
+            Level = q.front().second;
             q.pop();
-            if(node==nullptr)continue;
-          mp[Level].push_back(node->val);
-            for(int i=0;i<node->children.size();i++)
-                q.push({node->children[i],Level+1});
-        }        for (auto it: mp)
+            if (node == nullptr)continue;
+            mp[Level].push_back(node->val);
+            for (int i = 0; i < node->children.size(); i++)
+                q.push({node->children[i], Level + 1});
+        }
+        for (auto it: mp)
             ans.push_back(it.second);
         return ans;
     }
