d6bd04cdc8d8b97d15fa8d0d049164910dfefbb6
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-09-12 19:29:37 +0200

    Time: 11 ms (52.87%), Space: 10.7 MB (27.39%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+       int e=tokens.size()-1,score=0,mx=0;
+        sort(tokens.begin(),tokens.end());
+       for(int i=0;i<=e;i++){
+           
+           if(tokens[i]<=power)
+               power-=tokens[i],score++;
+           else if(score>0){
+               while(score>0&&power<tokens[i]&&e>i)
+                   power+=tokens[e],score--,e--;
+               if(power>=tokens[i]) 
+                   power-=tokens[i],score++;
+           }
+           else break;
+           mx=max(mx,score);
+       }
+        return mx;
+    }
+};
