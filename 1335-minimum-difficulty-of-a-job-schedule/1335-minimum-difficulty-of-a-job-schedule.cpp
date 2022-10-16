172b462a3ff75912c5753e80e21e648acd5b6137
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-10-16 21:07:13 +0200

    Time: 45 ms (83.97%), Space: 11.8 MB (10.19%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int dp[305][305][11];
-    vector<int>jobDifficulty;
-    int GetAns(int in,int mx,int d){
+    int GetAns(int in,int mx,int d,vector<int>&jobDifficulty){
         if(in==jobDifficulty.size())
             return d==0&&mx==-1?0:1e6;
         if(d<0)return 1e6;
@@ -11,12 +10,11 @@ public:
         int &ret=dp[in][mx][d];
         if(~ret)
             return ret;
-        ret=min(GetAns(in+1,-1,d-1)+jobDifficulty[mx],GetAns(in+1,mx,d));
+        ret=min(GetAns(in+1,-1,d-1,jobDifficulty)+jobDifficulty[mx],GetAns(in+1,mx,d,jobDifficulty));
         return ret;
     }
-    int minDifficulty(vector<int>& jobdifficulty, int d) {
-        jobDifficulty=jobdifficulty;
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
         memset(dp,-1,sizeof dp);
-        return GetAns(0,-1,d)>=1e6? -1:GetAns(0,0,d);
+        return GetAns(0,-1,d,jobDifficulty)>=1e6? -1:GetAns(0,0,d,jobDifficulty);
     }
 };
