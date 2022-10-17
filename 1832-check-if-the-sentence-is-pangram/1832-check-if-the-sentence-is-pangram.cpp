37987178d481007fb53dff56f57385d330f28f02
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-10-17 13:39:51 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (94.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int fre[26];
+    bool fre[26];
     bool checkIfPangram(string sentence) {
         for(int i=0;i<sentence.size();i++)
             if(sentence[i]!=' ')
-                fre[sentence[i]-'a']++;
+                fre[sentence[i]-'a']=1;
         for(int i=0;i<26;i++)
             if(!fre[i])
                 return false;
