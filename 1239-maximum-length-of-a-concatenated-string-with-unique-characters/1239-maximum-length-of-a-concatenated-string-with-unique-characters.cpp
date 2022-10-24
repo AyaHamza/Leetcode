d07262b9acc325c7252571997347cf222b933675
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2022-10-24 09:49:14 +0200

    Time: 2925 ms (5.05%), Space: 650.3 MB (5.05%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int maxLength(vector<string>& arr) {
-        int ans=0;
-      for(int i=1;i<=(1<<arr.size());i++)
-      {
-          string s;
-          int valid=1;
-          for(int j=0;j<arr.size();j++)
-              if((i>>j)&1)
-                  s+=arr[j];
-          vector<int>fre(26);
-          for(int j=0;j<s.size();j++ ){ 
+    int ans=0;
+    bool Check(string s){
+        vector<int>fre(26);
+         for(int j=0;j<s.size();j++ ){ 
               fre[s[j]-'a']++;
               if(fre[s[j]-'a']>1)
-                  valid=0;
+                  return 0;
           }
-          if(valid)
-              ans=max(ans,(int)s.size());
-      }
+        return 1;
+    }
+    void GetAns(int in,string s,vector<string>& arr){
+        if(in==arr.size()){
+            if(Check(s))
+                ans=max(ans,(int)s.size());
+            return;
+        }
+        GetAns(in+1,s+arr[in],arr);
+        GetAns(in+1,s,arr);       
+    }
+    int maxLength(vector<string>& arr) {
+     GetAns(0,"",arr);
         return ans;
     }
 };
