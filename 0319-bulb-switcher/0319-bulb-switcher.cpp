1b626958b0da3bf36887d4987315729bfa6b6f1c
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2023-04-27 02:47:42 +0200

    Time: 3 ms (45.34%), Space: 6 MB (63.99%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+         int ans=0;
+        for(int i=1;i*i<=n;i++)
+            if(i*i<=n)
+                ans++;
+        return ans;
+    
+    }
+};
