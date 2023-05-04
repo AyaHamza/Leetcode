fc660f22fd1bfb0f6150afcbdd99a0b2aba2d659
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2023-05-05 01:14:07 +0300

    Time: 7 ms (73.70%), Space: 8.1 MB (35.93%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     string predictPartyVictory(string senate) {
        queue<int>D,R;
-        int d,r,n=senate.size();
+        int d,r;
         for (int i = 0; i < senate.size(); i++)
             senate[i] == 'D' ? D.push(i) : R.push(i);
         while (D.size()&&R.size()){
             d=D.front(),r=R.front();
             R.pop(),D.pop();
-            d<r? D.push(d+n): R.push(r+n);
+            d<r? D.push(d+senate.size()): R.push(r+senate.size());
         }
         return R.size()? "Radiant":"Dire";
     }
