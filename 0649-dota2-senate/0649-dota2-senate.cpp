13e41bbd23e3a7cd93bac0fca860873e07782176
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2023-05-05 01:13:16 +0300

    Time: 4 ms (93.70%), Space: 8 MB (35.93%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -5,14 +5,10 @@ public:
         int d,r,n=senate.size();
         for (int i = 0; i < senate.size(); i++)
             senate[i] == 'D' ? D.push(i) : R.push(i);
-
         while (D.size()&&R.size()){
             d=D.front(),r=R.front();
             R.pop(),D.pop();
-            if(d<r)
-                D.push(d+n);
-            else 
-                R.push(r+n);
+            d<r? D.push(d+n): R.push(r+n);
         }
         return R.size()? "Radiant":"Dire";
     }
