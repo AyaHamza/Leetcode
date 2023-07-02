93a6ecc85fa8aec050dd0b732674664ab62d0b8b
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2023-07-02 10:02:18 +0300

    Time: 67 ms (97.06%), Space: 8.8 MB (75.49%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int arr[25];
-    int get_ans(int ind,int num_of_requests,int c0,vector<vector<int>>& requests,int n){
-        if (ind==requests.size())
-            return c0==n ? num_of_requests:0;
-        int x=0;
-        if(requests[ind][0]!=requests[ind][1]){
-        arr[requests[ind][0]]--,arr[requests[ind][1]]++;
-        if(arr[requests[ind][0]]==0) x++;
-        if(arr[requests[ind][1]]==1)x--;
+    
+    int get_ans(int ind,int num_of_requests,vector<vector<int>>& requests,int n){
+        if (ind==requests.size()){
+            for(int i=0;i<n;i++)
+                if(arr[i]!=0) return 0;
+            return num_of_requests;
         }
-        int mx=get_ans(ind+1,num_of_requests+1,c0+x,requests,n);
-        if(requests[ind][0]!=requests[ind][1])
+        
+        arr[requests[ind][0]]--,arr[requests[ind][1]]++;
+        int mx=get_ans(ind+1,num_of_requests+1,requests,n);
+        
         arr[requests[ind][0]]++,arr[requests[ind][1]]--;
-        mx=max(mx,get_ans(ind+1,num_of_requests,c0,requests,n));
+        mx=max(mx,get_ans(ind+1,num_of_requests,requests,n));
+        
         return mx;  
     }
     int maximumRequests(int n, vector<vector<int>>& requests) {
-        return get_ans(0,0,n,requests,n);
+        return get_ans(0,0,requests,n);
     }
 };
