f459a06374e60299ce5f651f6f1a6a2cbf973fca
AyaHamza <62474477+AyaHamza@users.noreply.github.com>
2023-07-05 05:12:05 +0300

    Time: 40 ms (83.38%), Space: 37.2 MB (5.59%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int prefix[100005],suffix[100005];
     int longestSubarray(vector<int>& nums) {
-        int c=0,ans=0;
+        int c=0,c2=0,ans=0;
         for(int i=0;i<nums.size();i++){
-            prefix[i]=c;
+            prefix[i]=c,suffix[nums.size()-i-1]=c2;
             nums[i]?c++:c=0;
+            nums[nums.size()-i-1]?c2++:c2=0;
         }
-        c=0;
-        for(int i=nums.size()-1;i>=0;i--){
-            suffix[i]=c;
-            nums[i]?c++:c=0;
-        }
-        for(int i=0;i<nums.size();i++){
-            cout<<suffix[i]<<" "<<prefix[i]<<endl;
+    
+        for(int i=0;i<nums.size();i++)
             ans=max(ans,prefix[i]+suffix[i]);
-        }
+        
         return ans;
     }
 };
